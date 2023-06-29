7e60a8a3712c2ab8a1eba465e56b083aac144c3f
Collection50 <fkaus65@naver.com>
2023-06-29 15:06:52 +0900

    style: 전역 스타일로 폰트 관리

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -3,7 +3,21 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
  * {
   margin: 0;
-  padding: 0
+  padding: 0;
+  background: black;
+
+  @font-face {
+  font-family: 'Pacifico', cursive;
+  font-style: normal;
+  src: url('./assets/Fonts/Pacifico-Regular.ttf') format('ttf');
+  }
+  
+  @font-face {
+  font-family: 'K2D', sans-serif;
+  font-style: normal;
+  src: url('./assets/Fonts/K2D-Bold.ttf') format('ttf');
+  }
+
   }
 `;
 
