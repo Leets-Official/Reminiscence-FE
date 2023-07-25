09d6ab22a09e6a3324663c59306a455f2c0871bb
Collection50 <fkaus65@naver.com>
2023-07-25 13:58:05 +0900

    feat: 회원가입 api 연결

--- src/api/core.js
+++ src/api/core.js
@@ -3,7 +3,7 @@ import { ACCESS_TOKEN, HTTP_METHODS } from '../constants';
 import { LocalStorage } from '../util/LocalStorage';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.REACT_APP_PUBLIC_API_URL,
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 });
